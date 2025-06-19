5cb4965ad2757f0cd98942c71748e3b2947f2a78
Tomlee Abila <tomleeabila@gmail.com>
2025-06-19 17:59:55 +0300

    Feat; creation of RegisterRoutes for registration of routes

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// RegisterRoutes adds all HTTP routes to the mux
+func RegisterRoutes(mux *http.ServeMux){
+	mux.HandleFunc("/register", RegisterHandler)
+}
+
+// Placeholder: /register
+func RegisterHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "Register endpoint is live")
+}
