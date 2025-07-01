69b6012268250e8a3fcae44faf3785af740806d4
vomolo <valentineomolo1@gmail.com>
2025-07-01 13:27:11 +0300

    feat: define Session struct to represent user sessions

--- backend/internal/utils/session.go
+++ backend/internal/utils/session.go
@@ -1,1 +1,13 @@
 package utils
+
+import (
+	"time"
+)
+
+// Session represents a user session
+type Session struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
