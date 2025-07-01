8eb8149263741e4e0d411cfb46cf2a0cf530f88b
vomolo <valentineomolo1@gmail.com>
2025-07-01 22:36:58 +0300

    refactor: remove redundant comment before Contains function

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -99,7 +99,6 @@ func (ul *UserLogin) Validate() error {
 }
 
 // Helper functions
-// Helper functions
 func Contains(slice []string, item string) bool {
 	for _, s := range slice {
 		if s == item {
