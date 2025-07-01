933f0f75f5de8a9e1b510565c08a5a1b6eb149a3
vomolo <valentineomolo1@gmail.com>
2025-07-01 13:02:37 +0300

    format: add user struct definition with JSON tags for serialization

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+)
+
+// User represents a user in the system
+type User struct {
+	ID        string    `json:"id"`
+	Nickname  string    `json:"nickname"`
+	Age       int       `json:"age"`
+	Gender    string    `json:"gender"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"` // Never include password in JSON responses
+	CreatedAt time.Time `json:"created_at"`
+}
