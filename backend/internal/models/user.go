c6997c9330fd9511fcee867cbedd8e86675e3181
vomolo <valentineomolo1@gmail.com>
2025-07-01 13:04:57 +0300

    format: add UserRegistration struct for user registration data

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -16,3 +16,14 @@ type User struct {
 	Password  string    `json:"-"` // Never include password in JSON responses
 	CreatedAt time.Time `json:"created_at"`
 }
+
+// UserRegistration represents the data needed for user registration
+type UserRegistration struct {
+	Nickname  string `json:"nickname"`
+	Age       int    `json:"age"`
+	Gender    string `json:"gender"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
