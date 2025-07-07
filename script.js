0997269a4946cfea5392732fc35b5b623c0adc19
Tomlee Abila <tomleeabila@gmail.com>
2025-07-08 02:13:10 +0300

    feat: messages and search bar script

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // SIDEBAR
 const menuItems = document.querySelectorAll('.menu-item');
 
+// MESSAGES
+const messagesNotification = document.querySelector
+    ('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = document.querySelectorAll('.message')
+const messageSearch = document.querySelector('#message-search')
+
+// THEME
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+
+// ==================== SIDEBAR ==========================
+
 // remove active class from all menu items
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -15,10 +28,39 @@ menuItems.forEach(item => {
         if (item.id != 'notifications') {
             document.querySelector('.notifications-popup').
                 style.display = 'none';
-        } else{
+        } else {
             document.querySelector('.notifications-popup').
                 style.display = 'block';
             document.querySelector('#notifications .notification-count').style.display = 'none';
         }
     })
 })
+
+
+// ==================== MESSAGES ==========================
+// searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    console.log(val);
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLocaleLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+// search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// hightlight messages card when messages menu items is clicked
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display
+        = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
