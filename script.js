9e9ae491707393322ac1519c73c901851d68a4e8
Tomlee Abila <tomleeabila@gmail.com>
2025-07-11 15:19:11 +0300

    feat: script for changing theme color

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
+
 
 // ==================== SIDEBAR ==========================
 
@@ -100,7 +105,7 @@ fontSizes.forEach(size => {
     size.addEventListener('click', () => {
         removeSizeSelector();
         let fontSize;
-        size.classList.toggle('active');
+        size.classList.add('active');
 
         if (size.classList.contains('font-size-1')) {
             fontSize = '10px';
@@ -123,9 +128,38 @@ fontSizes.forEach(size => {
             root.style.setProperty('----sticky-top-left', '-12rem');
             root.style.setProperty('----sticky-top-right', '-35rem');
         }
-        
-        console.log("Hello Tomlee")
+
         // change font size of the root html element
         document.querySelector('html').style.fontSize = fontSize;
+    })
+})
+
+// remove active class from color spans
+const removeActiveColor = () => {
+    colorPalette.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+
+// change primary colors
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        removeActiveColor();
+        let primaryHue;
+        if (color.classList.contains('color-1')) {
+            primaryHue = 252;
+        } else if (color.classList.contains('color-2')) {
+            primaryHue = 52;
+        } else if (color.classList.contains('color-3')) {
+            primaryHue = 352;
+        } else if (color.classList.contains('color-4')) {
+            primaryHue = 152;
+        } else if (color.classList.contains('color-5')) {
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue', primaryHue);
+
     })
 })
